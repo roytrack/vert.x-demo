86a096327b89ae5b0c596029abcfbc2934e5f141
ruanchangming <ruanchangming@lianjia.com>
2018-03-08 15:55:06 +0800

    eventBus

---
+++ learn/src/main/java/com/roytrack/vertx/core/EventBusTest.java
@@ -0,0 +1,40 @@
+package com.roytrack.vertx.core;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.MessageConsumer;
+
+/***
+ *
+ *   Created by roytrack on 2018-03-08  15:06
+ */
+public class EventBusTest extends AbstractVerticle {
+    public void start(){
+        EventBus eventBus=vertx.eventBus();
+        MessageConsumer<String> consumer=eventBus.consumer("news.uk.sport", message->{
+            System.out.println("I have received a message: "+message.body());
+        });
+        consumer.completionHandler(res ->{
+            if(res.succeeded()){
+                System.out.println("The handler registration has reached all nodes");
+            }else{
+                System.out.println("Registration failed!");
+            }
+        });
+
+        eventBus.publish("news.uk.sport","Hey! This is a message published!");
+        eventBus.send("news.uk.sport","Hey! send message!");
+
+        //consumer unregister
+//        consumer.unregister(res ->{
+//            if(res.succeeded()){
+//                System.out.println("The handler un-registration has reached all nodes");
+//            }else{
+//                System.out.println("Un-registration failed!");
+//            }
+//        });
+
+
+    }
+
+}
