8dfe00890dfcc78da48e5d95bd735d2fab10ff64
ruanchangming <ruanchangming@lianjia.com>
2018-03-08 16:12:10 +0800

    eventBus and transfer msg

--- learn/src/main/java/com/roytrack/vertx/core/EventBusTest.java
+++ learn/src/main/java/com/roytrack/vertx/core/EventBusTest.java
@@ -1,8 +1,13 @@
 package com.roytrack.vertx.core;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
+import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
+
+import java.util.Iterator;
+import java.util.Map;
 
 /***
  *
@@ -11,8 +16,19 @@ import io.vertx.core.eventbus.MessageConsumer;
 public class EventBusTest extends AbstractVerticle {
     public void start(){
         EventBus eventBus=vertx.eventBus();
+        DeliveryOptions options=new DeliveryOptions();
+        options.addHeader("some-header","some-value");
         MessageConsumer<String> consumer=eventBus.consumer("news.uk.sport", message->{
-            System.out.println("I have received a message: "+message.body());
+            System.out.println("I have received a message: "+message.body()+"| message header |");
+            MultiMap headers=message.headers();
+            if(null!=headers){
+                Iterator<Map.Entry<String, String>> itr=  headers.iterator();
+                while (itr.hasNext()){
+                    Map.Entry<String, String> entry=itr.next();
+                    System.out.println("key:"+entry.getKey()+"  value:"+entry.getValue());
+                }
+            }
+            message.reply("this msg received hey hey hey");
         });
         consumer.completionHandler(res ->{
             if(res.succeeded()){
@@ -23,7 +39,11 @@ public class EventBusTest extends AbstractVerticle {
         });
 
         eventBus.publish("news.uk.sport","Hey! This is a message published!");
-        eventBus.send("news.uk.sport","Hey! send message!");
+        eventBus.send("news.uk.sport","Hey! send message!",options,ar->{
+            if(ar.succeeded()){
+                System.out.println("message reply is "+ar.result().body());
+            }
+        });
 
         //consumer unregister
 //        consumer.unregister(res ->{
