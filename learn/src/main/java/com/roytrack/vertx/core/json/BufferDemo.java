2be8fff5134499ad48a0fb105ce10a04d741c1eb
roytrack <rcm8848@gmail.com>
2018-03-10 09:34:06 +0800

    use buffer a good start

---
+++ learn/src/main/java/com/roytrack/vertx/core/json/BufferDemo.java
@@ -0,0 +1,20 @@
+package com.roytrack.vertx.core.json;
+
+import io.vertx.core.buffer.Buffer;
+
+/**
+ * Created by roytrack on 2018/3/9 23:02.
+ */
+public class BufferDemo {
+    public static void main(String[] args) {
+        Buffer buffer=Buffer.buffer("a buffer","UTF-8");
+        buffer.appendByte((byte)23).appendInt(5).appendDouble(2.333d).appendString(" one day");
+        System.out.println(buffer.length());
+        Buffer buffer2=buffer.copy();
+        Buffer buffer3=buffer.slice(0,10);
+        System.out.println(buffer);
+        System.out.println(buffer2);
+        System.out.println(buffer3);
+
+    }
+}
